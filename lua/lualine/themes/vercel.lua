7c3107304032ca88e92f0ae212b8705250bbf00b
tiesen243 <ttien56906@gmail.com>
2025-04-07 08:21:21 +0700

    chore: change lualine text color

--- lua/lualine/themes/vercel.lua
+++ lua/lualine/themes/vercel.lua
@@ -4,43 +4,43 @@ local colors = require("vercel.colors")
 local M = {}
 
 M.normal = {
-	a = { fg = colors.mainText, bg = colors.syntaxConstant, gui = "bold" },
+	a = { fg = colors.editorBackground, bg = colors.syntaxConstant, gui = "bold" },
 	b = { fg = colors.syntaxConstant, bg = colors.editorBackground },
 	c = { fg = colors.mainText, bg = config.transparent and "NONE" or colors.popupBackground },
 }
 
 M.visual = {
-	a = { fg = colors.mainText, bg = colors.warningText, gui = "bold" },
+	a = { fg = colors.editorBackground, bg = colors.warningText, gui = "bold" },
 	b = { fg = colors.warningText, bg = colors.editorBackground },
 	c = { fg = colors.mainText, bg = config.transparent and "NONE" or colors.popupBackground },
 }
 
 M.inactive = {
-	a = { fg = colors.mainText, bg = colors.editorBackground, gui = "bold" },
+	a = { fg = colors.editorBackground, bg = colors.editorBackground, gui = "bold" },
 	b = { fg = colors.commentText, bg = colors.editorBackground },
 	c = { fg = colors.mainText, bg = config.transparent and "NONE" or colors.popupBackground },
 }
 
 M.replace = {
-	a = { fg = colors.mainText, bg = colors.errorText, gui = "bold" },
+	a = { fg = colors.editorBackground, bg = colors.errorText, gui = "bold" },
 	b = { fg = colors.errorText, bg = colors.editorBackground },
 	c = { fg = colors.mainText, bg = config.transparent and "NONE" or colors.popupBackground },
 }
 
 M.insert = {
-	a = { fg = colors.mainText, bg = colors.successText, gui = "bold" },
+	a = { fg = colors.editorBackground, bg = colors.successText, gui = "bold" },
 	b = { fg = colors.successText, bg = colors.editorBackground },
 	c = { fg = colors.mainText, bg = config.transparent and "NONE" or colors.popupBackground },
 }
 
 M.terminal = {
-	a = { fg = colors.mainText, bg = colors.syntaxFunction, gui = "bold" },
+	a = { fg = colors.editorBackground, bg = colors.syntaxFunction, gui = "bold" },
 	b = { fg = colors.syntaxFunction, bg = colors.editorBackground },
 	c = { fg = colors.mainText, bg = config.transparent and "NONE" or colors.popupBackground },
 }
 
 M.command = {
-	a = { fg = colors.mainText, bg = colors.syntaxKeyword, gui = "bold" },
+	a = { fg = colors.editorBackground, bg = colors.syntaxKeyword, gui = "bold" },
 	b = { fg = colors.syntaxKeyword, bg = colors.editorBackground },
 	c = { fg = colors.ma, bg = config.transparent and "NONE" or colors.popupBackground },
 }
